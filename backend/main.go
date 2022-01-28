47c0b19311099863e08c7894609b16be58f1108a
Richa Gupta <richa13102333@gmail.com>
2022-01-27 21:16:26 -0500

    [Issue #3][Milestone #1] Basic API to fetch the data. Created main.go file Created Get User Endpoint

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func getALL(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Get Data API User Endpoint Hit")
+}
+
+func putData(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Put Data API User Endpoint Hit")
+}
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./public")))
-	err := http.ListenAndServe(":3001", nil)
-	if err != nil {
-		panic("Error: " + err.Error())
-	}
+	fmt.Println("Launching GORM")
+	handleRequests()
+}
+
+func handleRequests() {
+	rtr := mux.NewRouter().StrictSlash(true)
+	rtr.HandleFunc("/", getALL).Methods("GET")
+	//rtr.HandleFunc("/user/", putData).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8081", rtr))
 }
