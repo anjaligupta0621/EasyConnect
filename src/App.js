e158ff2125e93231e1bca624ed19cf42a4868ee4
Anjali Gupta <anjaligupta0621@gmail.com>
2022-02-01 21:22:12 -0500

    Added navigation and Home-template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Navbar from "./components/navbar.component"
+// import AuthCredentials from "./components/create-user.component";
+import Home from "./components/home.component";
 import './App.css';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          // <Navbar/>
+          <Router>
+            <Navbar/>
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Router>
   );
 }
 
---
+++ src/components/home.component.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import './home.component.css'
+
+class Home extends React.PureComponent {
+  render() {
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <br/>
+        </div>
+      );
+  }
+}
+
+export default Home;
