05f389edfb17c3d0f885d5f88ac7040f19dcf84f
Anjali Gupta <anjaligupta0621@gmail.com>
2022-03-03 02:29:00 -0500

    Adding backend init

--- src/components/postJobDashBoard.component.js
+++ src/components/postJobDashBoard.component.js
@@ -1,4 +1,4 @@
-import { React } from "react";
+import React from "react";
 import '../css/styles.css';
 import Header from "./header.component";
 import './postJobDashBoard.component.css';
@@ -27,44 +27,54 @@ const PostJobDashBoard = (props) => {
 					</thead>
 					<tbody>
 						<tr>
-							<td className="no-display">23 Nov '17</td>
-							<td className="full-width">Business Development, Sales ( <a href="#">View</a> )</td>
-							<td >
-								<div className="toggle_bt">
-									<div className="onoffswitch">
-										{/* <input type="checkbox" checked="" class="onoffswitch-checkbox" id="onoffswitch1"> */}
-										<label for="onoffswitch1" class="onoffswitch-label"><span class="onoffswitch-inner"></span><span class="onoffswitch-switch"></span></label>
-        
-                                    </div>
-								</div>
-							</td>
-                            <td></td>
-                            <td>7 Dec  '17 </td>
-
-							
+							<th className="hdr-postdt">Posting Date</th>
+							<th>Role</th>
+							<th>Status</th>
+							<th><span className="hidden-xs">No. of</span> Applicants</th>
+							<th>Deadline</th>
 						</tr>
-						
-						<tr className="closed">
-							<td className="no-display">23 Nov  '17</td>
-							<td className="full-width">Business Development, Sales ( <a href="#">View</a> )</td>
-							<td>
-								<div className="toggle_bt">
-									<div className="onoffswitch">
-										<label for="onoffswitch4" class="onoffswitch-label"><span class="onoffswitch-inner"></span><span class="onoffswitch-switch"></span></label>
+						</thead>
+						<tbody>
+							<tr>
+								<td className="no-display">23 Nov '17</td>
+								<td className="full-width">Business Development, Sales ( <a href="#">View</a> )</td>
+								<td >
+									<div className="toggle_bt">
+										<div className="onoffswitch">
+											{/* <input type="checkbox" checked="" className="onoffswitch-checkbox" id="onoffswitch1"> */}
+											<label htmlFor="onoffswitch1" className="onoffswitch-label"><span className="onoffswitch-inner"></span><span className="onoffswitch-switch"></span></label>
+			
+										</div>
 									</div>
-								</div>
-							</td>
-							<td></td>
-							<td>7 Dec  '17 </td>
-						</tr>
-						
-					</tbody>
-				</table>
-				
+								</td>
+								<td></td>
+								<td>7 Dec  '17 </td>
+
+								
+							</tr>
+							
+							<tr className="closed">
+								<td className="no-display">23 Nov  '17</td>
+								<td className="full-width">Business Development, Sales ( <a href="#">View</a> )</td>
+								<td>
+									<div className="toggle_bt">
+										<div className="onoffswitch">
+											<label htmlFor="onoffswitch4" className="onoffswitch-label"><span className="onoffswitch-inner"></span><span className="onoffswitch-switch"></span></label>
+										</div>
+									</div>
+								</td>
+								<td></td>
+								<td>7 Dec  '17 </td>
+							</tr>
+							
+						</tbody>
+					</table>
+					
+				</div>
 			</div>
-		</div>
-        </div>
-    )
+			</div>
+		)
+	}
 }
 
 export default PostJobDashBoard;
