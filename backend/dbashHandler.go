2e74de4410f60f772226be481af12e0cccb7e7a6
gau4x4 <50134400+gau4x4@users.noreply.github.com>
2022-03-02 23:24:16 -0500

    Mockup function for Dashboard Handling
    Methods to handle dashboard activities.

--- backend/dbashHandler.go
+++ backend/dbashHandler.go
@@ -54,10 +54,58 @@ func getDashJobs(w http.ResponseWriter, r *http.Request) {
 
 func deleteDashJob(w http.ResponseWriter, r *http.Request) {
 	setupCorsResponse(&w, r)
-	//TBD
+	type Recruiter_struct struct {
+		Recruiter_ID uint
+	}
+	db, err := gorm.Open("sqlite3", "RecruiterDetails.db")
+	if err != nil {
+		panic("Failed to connect database")
+	}
+	defer db.Close()
+
+	decoder := json.NewDecoder(r.Body)
+	var recruiter_id Recruiter_struct
+
+	err2 := decoder.Decode(&recruiter_id)
+	if err2 != nil {
+		panic(err2)
+	}
+	var jobs []Job
+	db.Table("jobs").Where("Recruiter_ID = ?", recruiter_id.Recruiter_ID).Find(&jobs)
+	if jobs != nil {
+		for i := 0; i < len(jobs); i++ {
+			json.NewEncoder(w).Encode(jobs[i])
+		}
+	} else {
+		json.NewEncoder(w).Encode("No Jobs found!")
+	}
 }
 
 func updateDashJob(w http.ResponseWriter, r *http.Request) {
 	setupCorsResponse(&w, r)
-	//TBD
+	type Recruiter_struct struct {
+		Recruiter_ID uint
+	}
+	db, err := gorm.Open("sqlite3", "RecruiterDetails.db")
+	if err != nil {
+		panic("Failed to connect database")
+	}
+	defer db.Close()
+
+	decoder := json.NewDecoder(r.Body)
+	var recruiter_id Recruiter_struct
+
+	err2 := decoder.Decode(&recruiter_id)
+	if err2 != nil {
+		panic(err2)
+	}
+	var jobs []Job
+	db.Table("jobs").Where("Recruiter_ID = ?", recruiter_id.Recruiter_ID).Find(&jobs)
+	if jobs != nil {
+		for i := 0; i < len(jobs); i++ {
+			json.NewEncoder(w).Encode(jobs[i])
+		}
+	} else {
+		json.NewEncoder(w).Encode("No Jobs found!")
+	}
 }
