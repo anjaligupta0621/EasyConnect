3122b3b801b89769208dabc6b0678e01b147b029
Shashankchauhan-10 <shashankchauhan1994@gmail.com>
2022-03-04 00:23:43 -0500

    UI cypress testing changes
    Adding LogIn, LogOut,Post Job Component testing using Cypress.

--- cypress/integration/uitesting/login_spec.js
+++ cypress/integration/uitesting/login_spec.js
@@ -4,8 +4,8 @@
 describe('Login page', () => {
   
   it('Logs in user', () => {
-    const username = "dummy@gmail.com"
-    const password ="dummy"
+    const username = "anjaligupta@ufl.edu"
+    const password ="check123"
 
     cy.visit('http://localhost:3000/')
     cy.get('#btnlogin').click({force: true})
@@ -13,15 +13,18 @@ describe('Login page', () => {
    
     cy.get('input[name=emailId]').type(username)
     cy.get('input[name=Password]').type(password)
-    cy.get('input[type=submit]').click()
-    cy.contains('Logout')
+    cy.get('input[type=submit].text-input').click()
+    cy.contains('Sign Out')
+      //cy.url().should('include', '/login')
+     
+      
+     // cy.contains('Sign In')
     
   })
   it('displays logout button',() => {
-    cy.get('[data-cy=logoutbtn]').contains('Logout')
+    cy.get('li>a').contains('Sign Out')
   })
   it('shows chat component',() => {
-    cy.get('div').contains('Chat')
-    
+    cy.get('a').contains('Sign Out')
   })
 })
