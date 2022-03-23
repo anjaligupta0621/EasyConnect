f30980b8e85ff72fc885368cd6672212b64b8570
gau4x4 <50134400+gau4x4@users.noreply.github.com>
2022-03-23 19:06:51 -0400

    Sprint3[Issue #75] Revamping the Style.
    Revamping the Styling for Util and Test.

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -1,80 +1,117 @@
-import React from 'react'
+import React from "react";
 
-import '../css/styles.css';
-import '../css/main.css';
-import '../css/menu.css';
-import '../css/login-modal-wizard.css';
-import './header.component.css';
+import "../css/styles.css";
+import "../css/main.css";
+import "../css/menu.css";
+import "../css/login-modal-wizard.css";
+import "./header.component.css";
 // import { useNavigate } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
+import Navbar from "./navbar.component.js";
+import recruiterLogo from "../img/recruiter.png";
+import jobseekerLogo from "../img/jobseeker.png";
 
-import Navbar from "./navbar.component.js" 
-import recruiterLogo from "../img/recruiter.png"
-import jobseekerLogo from "../img/jobseeker.png"
-
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 
 const Header = (props) => {
+  const postJob = () => {
+    props.showLogin();
+  };
 
-    const postJob = () => {
-        props.showLogin();
-    }
+  const gotoHome = () => {
+    // debugger;
+    props.history.push("/home");
+  };
 
-    const gotoHome = () => {
-        // debugger;
-        props.history.push('/home');
-    }
-   
-    return (
-        <header>
-            <div className="top-select">
-                <div className="viewas">
-                    <div className="dropdown">
-                        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src={recruiterLogo}/> &nbsp;view as Recruiter </button>
-                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton"> <a className="dropdown-item" href="#"> <img src={jobseekerLogo}/> &nbsp;view as Job seeker</a> </div>
-                    </div>
-                </div>
+  return (
+    <header>
+      <div className="top-select">
+        <div className="viewas">
+          <div className="dropdown">
+            <button
+              className="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <img src={recruiterLogo} /> &nbsp;view as Recruiter{" "}
+            </button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              {" "}
+              <a className="dropdown-item" href="#">
+                {" "}
+                <img src={jobseekerLogo} /> &nbsp;view as Job seeker
+              </a>{" "}
             </div>
-            <nav className="navbar navbar-inverse">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar"> <span className="icon-bar"></span><span className="icon-bar"> </span> <span className="icon-bar"></span> </button>
-                        <div className="logo main-logo-box">
-                             {/* <a href="#"> <img src={logo} alt="" /></a> */}
-                             <span className="main-logo"></span>
-                             <div className="logo-name">
-                                 <span>EASY</span>
-                                 <span>CONNECT</span>
-                             </div>
-                        </div>
-                    </div>
+          </div>
+        </div>
+      </div>
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button
+              type="button"
+              className="navbar-toggle"
+              data-toggle="collapse"
+              data-target="#myNavbar"
+            >
+              {" "}
+              <span className="icon-bar"></span>
+              <span className="icon-bar"> </span>{" "}
+              <span className="icon-bar"></span>{" "}
+            </button>
+            <div className="logo main-logo-box">
+              {/* <a href="#"> <img src={logo} alt="" /></a> */}
+              <span className="main-logo"></span>
+              <div className="logo-name">
+                <span>EASY</span>
+                <span>CONNECT</span>
+              </div>
+            </div>
+          </div>
 
-                    <div className="collapse navbar-collapse" id="myNavbar">
-                        <ul className="nav navbar-nav">
-                        
-                            <li className="active"><Link to="/">Home</Link></li>
-                            {
-                                (props.isLoggedIn || global.isLoggedIn) ?
-                                <li><Link to="/jobDashboard">Post Job Description</Link></li> :
-                                <li><button onClick={postJob}>Post Job Description</button></li>
-                            }
-                            <li><a href="#">Shortlist Candidates</a></li>
-                        </ul>
-                        <ul className="nav navbar-nav navbar-right">
-                            <li>
-                                {
-                                    (!props.isLoggedIn && !global.isLoggedIn) ? 
-                                    <input name="" type="submit" value="Sign In" className="sign-in-bt-top" id="btnlogin" onClick={props.showLogin} /> :
-                                    // <input name="" type="submit" value="Sign Out" className="sign-in-bt-top" id="btnlogout" onClick={props.signOut} />
-                                    <a href='\' >Sign Out</a>
-                                }
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    )
-}
-export default Header
+          <div className="collapse navbar-collapse" id="myNavbar">
+            <ul className="nav navbar-nav">
+              <li className="active">
+                <Link to="/">Home</Link>
+              </li>
+              {props.isLoggedIn || global.isLoggedIn ? (
+                <li>
+                  <Link to="/jobDashboard">Post Job Description</Link>
+                </li>
+              ) : (
+                <li>
+                  <button onClick={postJob}>Post Job Description</button>
+                </li>
+              )}
+              <li>
+                <a href="#">Shortlist Candidates</a>
+              </li>
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              <li>
+                {!props.isLoggedIn && !global.isLoggedIn ? (
+                  <input
+                    name=""
+                    type="submit"
+                    value="Sign In"
+                    className="sign-in-bt-top"
+                    id="btnlogin"
+                    onClick={props.showLogin}
+                  />
+                ) : (
+                  // <input name="" type="submit" value="Sign Out" className="sign-in-bt-top" id="btnlogout" onClick={props.signOut} />
+                  <a href="\">Sign Out</a>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+export default Header;
--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,130 +1,158 @@
 /* eslint-disable no-undef */
-import React from 'react';
-import { Modal,Button } from 'react-bootstrap';
-import '../css/main.css';
-import '../css/menu.css';
-import '../css/styles.css';
-import '../css/login-modal-wizard.css';
-import './home.component.css'
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+import "../css/main.css";
+import "../css/menu.css";
+import "../css/styles.css";
+import "../css/login-modal-wizard.css";
+import "./home.component.css";
 import LoginModal from "./login.component.js";
 
-import Header from "./header.component.js"
-import logolarge from '../img/logo-large.png'
-import bannerArrow from '../img/banner-arrow.png'
-import shadowImage from '../img/steps-shadow.png'
+import Header from "./header.component.js";
+import logolarge from "../img/logo-large.png";
+import bannerArrow from "../img/banner-arrow.png";
+import shadowImage from "../img/steps-shadow.png";
 
 class Home extends React.PureComponent {
-
 	state = {
 		showModal: false,
-    isLoggedIn:false
-	}
+		isLoggedIn: false,
+	};
 
-  showLoginDialog =()=> {
-    this.setState({
-      showModal:true
-    })
-  }
+	showLoginDialog = () => {
+		this.setState({
+			showModal: true,
+		});
+	};
 
-  signOut = () => {
-    // debugger;
-    alert("shahsank");
-    this.setIsLoggedIn(false);
-    global.isLoggedIn = false;
+	signOut = () => {
+		// debugger;
+		alert("shahsank");
+		this.setIsLoggedIn(false);
+		global.isLoggedIn = false;
+	};
 
-  }
+	hideLoginDialog = () => {
+		this.setState({
+			showModal: false,
+		});
+	};
 
-  hideLoginDialog =()=> {
-    this.setState({
-      showModal:false
-    })
-  }
-
-  setIsLoggedIn = (isLoggedIn) => {
-    // debugger;
-    this.props.log(true);
-    // debugger;
-    this.setState({
-      isLoggedIn: isLoggedIn
-    })
-  }
+	setIsLoggedIn = (isLoggedIn) => {
+		// debugger;
+		this.props.log(true);
+		// debugger;
+		this.setState({
+			isLoggedIn: isLoggedIn,
+		});
+	};
 
 	render() {
-		const  mystyle = {  
-      padding: "0px",
-      backgroundImage:`url(${shadowImage})`,
-      backgroundRepeat:"no-repeat",
-      backgroundPosition:"top left"     
-      
-    };
+		const mystyle = {
+			padding: "0px",
+			backgroundImage: `url(${shadowImage})`,
+			backgroundRepeat: "no-repeat",
+			backgroundPosition: "top left",
+		};
 		const blockStyle = {
 			borderRadisu: "5px",
-
 		};
 		return (
-			< div>
-      { this.state.showModal? <LoginModal hideLogin={this.hideLoginDialog} setIsLoggedIn={this.setIsLoggedIn} /> : null }
+			<div className="home-component">
+				{this.state.showModal ? (
+					<LoginModal
+						hideLogin={this.hideLoginDialog}
+						setIsLoggedIn={this.setIsLoggedIn}
+					/>
+				) : null}
 
-      <Header showLogin={this.showLoginDialog} isLoggedIn={this.state.isLoggedIn} signOut={this.signOut} setIsLoggedIn={this.setIsLoggedIn} />
-			<div id="banner" className="text-center">
-      <h1><img src={logolarge} alt="Easy Connect"/>Easy <span>Connect</span></h1> 
-      <p className="caption1">We help you find the <span>Right and Job-ready Candidates</span></p>
-      <p className="caption2">Completely <span>FREE.</span> No hidden charges. <span>No credit card required</span></p>
+				<Header
+					showLogin={this.showLoginDialog}
+					isLoggedIn={this.state.isLoggedIn}
+					signOut={this.signOut}
+					setIsLoggedIn={this.setIsLoggedIn}
+				/>
+				<div id="banner" className="text-center">
+					<h1>
+						<img src={logolarge} alt="Easy Connect" />
+						Easy <span>Connect</span>
+					</h1>
+					<p className="caption1">
+						We help you find the <span>Right and Job-ready Candidates</span>
+					</p>
+					<p className="caption2">
+						Completely <span>FREE.</span> No hidden charges.{" "}
+						<span>No credit card required</span>
+					</p>
 
+					<div className="col-lg-12 steps home-grid-container">
+						<a href="#section1" className="home-cards">
+							<div className="col-lg-12 step1">
+								<div className="col-lg-4 col-md-4 col-sm-4 col-xs-12 step-no">
+									01
+								</div>
+								<div className="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+									<h2>Access a large pool of freshers</h2>
+									<p>
+										Access 25000+ freshers from across 500+ colleges in India
+									</p>
+								</div>
+							</div>
+						</a>
 
-  <div className="col-lg-12 steps home-grid-container" >
-  <a href="#section1" className="home-cards">	
-		<div className="col-lg-12 step1">
-			<div className="col-lg-3 col-md-3 col-sm-3 col-xs-12 step-no">01</div>
-			<div className="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-				<h2>Access a large pool of freshers</h2>
-				<p>Access 25000+ freshers from across 500+ colleges in India</p>
+						<a href="#section2" className="home-cards">
+							<div className="col-lg-12 step1">
+								<div className="col-lg-4 col-md-4 col-sm-4 col-xs-12 step-no">
+									02
+								</div>
+								<div className="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+									<h2>Applicants are automatically sorted</h2>
+									<p>
+										Interested Applicants will be automatically sorted by
+										Employability Skills
+									</p>
+								</div>
+							</div>
+						</a>
+
+						<a href="#section3" className="home-cards">
+							<div className="col-lg-12 step1">
+								<div className="col-lg-4 col-md-4 col-sm-4 col-xs-12 step-no">
+									03
+								</div>
+								<div className="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+									<h2>View rich profiles of top ranked candidates</h2>
+									<p>
+										Listen to the audio profiles of top ranked candidates.
+										Shortlist candidates who fit your Organization’s culture
+									</p>
+								</div>
+							</div>
+						</a>
+
+						<a href="#section4" className="home-cards">
+							<div className="col-lg-12 step1">
+								<div className="col-lg-4 col-md-4 col-sm-4 col-xs-12 step-no">
+									04
+								</div>
+								<div className="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+									<h2>Hire freshers without leaving your office</h2>
+									<p>
+										Hire only the best freshers directly from your Office
+										without the expense of visiting Campuses
+									</p>
+								</div>
+							</div>
+						</a>
+					</div>
+
+					<div className="banner-arrow text-center">
+						<a href="#section1">
+							<img src={bannerArrow} alt="" />
+						</a>
+					</div>
+				</div>
 			</div>
-		</div>
-	</a>
-
-  <a href="#section2" className="home-cards">	
-		<div className="col-lg-12 step1">
-			<div className="col-lg-3 col-md-3 col-sm-3 col-xs-12 step-no">02</div>
-			<div className="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-				<h2>Applicants are automatically sorted</h2>
-				<p>Interested Applicants will be automatically sorted by Employability Skills</p>
-			</div>
-		</div>
-	</a>
-	
-	<a href="#section3" className="home-cards">	
-		<div className="col-lg-12 step1" >
-			<div className="col-lg-3 col-md-3 col-sm-3 col-xs-12 step-no">03</div>
-			<div className="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-				<h2>View rich profiles of top ranked candidates</h2>
-				<p>Listen to the audio profiles of top ranked candidates. Shortlist candidates who fit your Organization’s culture</p>
-			</div>
-		</div>
-	</a>
-
-	<a href="#section4" className="home-cards">	
-		<div className="col-lg-12 step1">
-			<div className="col-lg-3 col-md-3 col-sm-3 col-xs-12 step-no">04</div>
-			<div className="col-lg-9 col-md-9 col-sm-9 col-xs-12">
-
-				<h2>Hire freshers without leaving your office</h2>
-				<p>Hire only the best freshers directly from your Office without the expense of visiting Campuses</p>
-			</div>
-		</div>
-	</a>
-
-  </div>
-
-  <div className="banner-arrow text-center">
-      <a href="#section1"><img src={bannerArrow} alt=""/></a>
-  </div>
-</div>
-  </div>
-
-		
-
-
 		);
 	}
 }
