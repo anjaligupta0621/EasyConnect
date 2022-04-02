9b4841c049f586143330fc3dc142f038cdfdbb27
gau4x4 <50134400+gau4x4@users.noreply.github.com>
2022-04-01 20:30:43 -0400

    Post Job Cypress Unit test #120

--- cypress/integration/uitesting/postjob_spec.js
+++ cypress/integration/uitesting/postjob_spec.js
@@ -1,36 +1,33 @@
 /// <reference types="cypress" />
 
 describe("PostJob page", () => {
-  it("Checks Logs in  user", () => {
-    const username = "ajeet1@gmail.com";
-    const password = "676767";
-    const orgName = "Google.com";
-    const website = "http://www.google.com";
-    const description =
-      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..";
-    const roleName = "SDE II";
-    const responsibilities =
-      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..";
-    const salaryFrom = 30000;
-    const salaryTo = 70000;
+  const username = "ajeet1@gmail.com";
+  const password = "676767";
+  const orgName = "Google.com";
+  const website = "http://www.google.com";
+  const description =
+    "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..";
+  const roleName = "SDE II";
+  const responsibilities =
+    "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..";
+  const salaryFrom = 30000;
+  const salaryTo = 70000;
 
+  it("Checks Logs in user", () => {
     cy.visit("http://localhost:3000/");
     cy.get("#btnlogin").click({ force: true });
     cy.get("input[name=emailId]").type(username);
     cy.get("input[name=Password]").type(password);
     cy.get("input[type=submit].text-input").click();
     cy.contains("Sign Out");
-
     cy.contains("Post Job Description").click({ force: true });
     cy.get(".postJob-anchor").click();
-
     // cy.contains("Post Job").click({ force: true });
     cy.url().should("include", "/postJob");
     cy.get("input[name=organization_name]").type(orgName);
     cy.get("input[name=organization_website]").type(website);
     cy.get("[name=orgDescription]").type(description);
     cy.contains("Next").click({ force: true });
-
     cy.get("input[name=role_name]").type(roleName);
     cy.get("input[name=question]").first().check();
     cy.get("input[name=jobtype]").last().check();
@@ -41,10 +38,8 @@ describe("PostJob page", () => {
     cy.get(".post-button").click();
     cy.contains("Dashboard");
     cy.url().should("include", "/jobDashBoard");
-
     cy.contains("Sign Out");
   });
-
   it("Displays logout button", () => {
     cy.contains("Sign Out");
   });
@@ -54,7 +49,7 @@ describe("PostJob page", () => {
     cy.get("th").contains("Posting Date");
   });
 
-  it("Show Jobs", () => {
+  it("Signing Out", () => {
     cy.contains("Sign Out").click({ force: true });
   });
 });
