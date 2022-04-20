1852c18f3c96d7ded82025f848a3831f74181191
Richa Gupta <richa13102333@gmail.com>
2022-04-20 12:08:19 +0200

    [Sprint 4][Unit Test] Test cases added for getCurrentRecruiter API for backend.

--- backend/endpoints_test.go
+++ backend/endpoints_test.go
@@ -20,6 +20,7 @@ func Router() *mux.Router {
 	router.HandleFunc("/login", handler.GetUsers).Methods("POST")
 	router.HandleFunc("/signup", handler.PutUserData).Methods("POST")
 	router.HandleFunc("/logout", handler.LogOut).Methods("POST")
+	router.HandleFunc("/getCurrentRecruiter", handler.GetCurrentUser).Methods("POST")
 	return router
 }
 
@@ -96,6 +97,41 @@ func TestLogOut(t *testing.T) {
 	fmt.Println(token.UserName)
 	jsonPayload, _ = json.Marshal(token)
 	request, _ = http.NewRequest("POST", "/logout", bytes.NewBuffer(jsonPayload))
+	response = httptest.NewRecorder()
+	Router().ServeHTTP(response, request)
+	assert.Equal(t, 200, response.Code, "OK response is expected")
+}
+
+func TestGetCurrentUser(t *testing.T) {
+
+	type User struct {
+		User  string
+		Token string
+	}
+	var tokenm User
+	tokenm.User = "test2"
+	tokenm.Token = "RandomlyGenerated"
+	jsonPayload, _ := json.Marshal(tokenm)
+	request, _ := http.NewRequest("POST", "/getCurrentRecruiter", bytes.NewBuffer(jsonPayload))
+	response := httptest.NewRecorder()
+	Router().ServeHTTP(response, request)
+	assert.Equal(t, 400, response.Code, "OK response is not expected")
+
+	db, err := gorm.Open("sqlite3", "RecruiterDetails.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+	var token User
+	var ut models.Usertoken
+	db.Table("usertokens").Where("Email = ?", tokenm.User).Find(&ut)
+	token.Token = ut.Token
+	token.User = ut.Email
+	fmt.Println(token.Token)
+	fmt.Println()
+	fmt.Println(token.User)
+	jsonPayload, _ = json.Marshal(token)
+	request, _ = http.NewRequest("POST", "/getCurrentRecruiter", bytes.NewBuffer(jsonPayload))
 	response = httptest.NewRecorder()
 	Router().ServeHTTP(response, request)
 	assert.Equal(t, 200, response.Code, "OK response is expected")
