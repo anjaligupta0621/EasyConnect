381d0813779fc6d80956332db4499cfda3e69f34
Shashank Kumar <shashankchauhan1994@gmail.com>
2022-04-20 12:31:54 +0200

    Add the DashBoard for Applicants

--- src/components/allApplicants.component.js
+++ src/components/allApplicants.component.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Header from "./header.component";
 import axios from "axios";
-import "./shortlistCandidate.component.css";
 import grammerImage from "../img/grammer.png";
 import shortlist from "../img/shortlist-candidate.svg";
 import downExcel from "../img/excel-download.svg";
@@ -12,116 +11,34 @@ import speakingImage from "../img/speaking.png";
 import noCandidateImage from "../img/candidate-no-img.svg";
 
 class AllApplicants extends React.Component {
-  state = {
-    shortlistCandidates: [],
-    recruiterID: localStorage.getItem("recruiterId"),
-  };
 
-  componentDidMount() {
-    var raw = JSON.stringify({
-      Recruiter_ID: parseInt(localStorage.getItem("recruiterId")),
-    });
-    //debugger;
-
-    fetch(`http://localhost:8081/getCandidatesByRecruiterId`, {
-      body: raw,
-      method: "POST",
-      mode: "cors",
-    })
-      .then((response) => response.json())
-      .then((result) => {
+    state = {
+        applliedCandidates: [],
+        recruiterID: localStorage.getItem("recruiterId"),
+      };
+      
+    componentDidMount() {
+        var raw = JSON.stringify({
+          Recruiter_ID: parseInt(localStorage.getItem("recruiterId")),
+        });
         //debugger;
-        this.setState({ shortlistCandidates: result });
-        console.log(this.state.shortlistCandidates);
-      })
-      .catch((error) => console.log("error", error));
-  }
-
+    
+        fetch(`http://localhost:8081/getCandidatesByRecruiterId`, {
+          body: raw,
+          method: "POST",
+          mode: "cors",
+        })
+          .then((response) => response.json())
+          .then((result) => {
+            //debugger;
+            this.setState({ applliedCandidates: result });
+            console.log(this.state.applliedCandidates);
+          })
+          .catch((error) => console.log("error", error));
+      }
   render() {
     return (
-      <div className="shortList-Component">
-        <Header />
-
-        <div class="main-wrapper col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          <div class="body-area col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <h1>Shortlist Candidates</h1>
-            <ol class="breadcrumb breadcrumb-arrow">
-              <li>
-                <a href="#">All Job Descriptions</a>
-              </li>
-            </ol>
-
-            <div id="Job-Filters" class="col-lg-3 col-md-4 col-sm-12 col-xs-12">
-              <div class="lhs-filters">
-                <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-6 legend">
-                  <span class="dot-excellent"></span>{" "}
-                  <p>
-                    Excellent{" "}
-                    <span class="hidden-xs hidden-sm">(97.5 – 100 %)</span>
-                  </p>
-                </div>
-                <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-6 legend">
-                  <span class="dot-verygood"></span>{" "}
-                  <p>
-                    Very Good{" "}
-                    <span class="hidden-xs hidden-sm"> (87.5 – 97.5 %)</span>
-                  </p>
-                </div>
-                <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-6 legend">
-                  <span class="dot-good"></span>{" "}
-                  <p>
-                    Good <span class="hidden-xs hidden-sm">(75 – 87.5 %)</span>
-                  </p>
-                </div>
-                <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-6 legend">
-                  <span class="dot-above-average"></span>{" "}
-                  <p>
-                    Above Average{" "}
-                    <span class="hidden-xs hidden-sm">(75 – 87.5 %)</span>
-                  </p>
-                </div>
-                <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-6 legend">
-                  <span class="dot-average"></span>{" "}
-                  <p>
-                    Average{" "}
-                    <span class="hidden-xs hidden-sm">(50 – 62.5 %)</span>
-                  </p>
-                </div>
-                <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-6 legend">
-                  <span class="dot-below-average"></span>{" "}
-                  <p>
-                    Below Average{" "}
-                    <span class="hidden-xs hidden-sm">(below 50th %)</span>
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-9 col-md-8 col-sm-12 col-xs-12">
-              <div id="postjob">
-                <div class="appl-info">
-                  You are viewing{" "}
-                  <span>
-                    all {this.state.shortlistCandidates.length} applicants
-                  </span>{" "}
-                  for the role
-                </div>
-                <div class="container-fluid">
-                  <ul class="nav nav-pills">
-                    <li class="active">
-                      <a data-toggle="pill" href="#all-applicants">
-                        All Applicants{" "}
-                        <span>({this.state.shortlistCandidates.length})</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a data-toggle="pill" href="#shortlist">
-                        Shortlisted (0)
-                      </a>
-                    </li>
-                  </ul>
-                  <div class="tab-content">
-                    <div id="all-applicants" class="tab-pane fade in active">
+        <div id="all-applicants" class="tab-pane fade in active">
                       <div id="Sort-By">
                         <h3>Sort by:</h3>
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 sortby">
@@ -166,7 +83,7 @@ class AllApplicants extends React.Component {
                           (The Employability Skills of these candidates has been
                           verified using background check)
                         </p>
-                        {this.state.shortlistCandidates.map(
+                        {this.state.applliedCandidates.map(
                           (candidate, index) => (
                             <div class="block-candidates-display" key={index}>
                               <div class="col-lg-5">
@@ -227,19 +144,10 @@ class AllApplicants extends React.Component {
                         )}
                       </div>
                       <div class="clearfix"></div>
-                    </div>
-                    <div id="shortlist" class="tab-pane fade">
-								
-							Shortlisted (30)								
-								
-					</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+                    </div>     
+
+                  
+                 
     );
   }
 }
